02eff771358ec1c0b54f7815fdcaa364ba3f91e6
Vinoth Rajendran <r.vinoth@live.com>
2018-10-13 11:05:15 +0530

    Added Bot basic info

---
+++ Middleware/Middleware/HelloBot.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+namespace Middleware
+{
+    public class HelloBot : IBot
+    {
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = new CancellationToken())
+        {
+            if (turnContext.Activity.Type is ActivityTypes.Message)
+            {
+                await turnContext.SendActivityAsync("Hello Welcome to Bot new 4.0 Sample", cancellationToken: cancellationToken);
+            }
+        }
+    }
+}
--- Middleware/Middleware/Startup.cs
+++ Middleware/Middleware/Startup.cs
@@ -5,30 +5,44 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Bot.Builder.BotFramework;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Middleware
 {
     public class Startup
     {
+        private IHostingEnvironment _environment;
+        public Startup(IHostingEnvironment environment)
+        {
+            _environment = environment;
+        }
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var buildConfig = new ConfigurationBuilder()
+                .SetBasePath(_environment.ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+
+            var config = buildConfig.Build();
+            services.AddSingleton(config);
+
+            services.AddBot<HelloBot>(serviceBot =>
+            {
+                serviceBot.CredentialProvider = new ConfigurationCredentialProvider(config);
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseStaticFiles();
+            app.UseBotFramework();
         }
     }
 }
