71bc91ba759da28825ada1312e374863bd99c4bd
Vinoth Rajendran <r.vinoth@live.com>
2018-10-14 10:25:32 +0530

    BotState(memory storage) sample

---
+++ BotStateSample/BotStateSample/BotStateDemo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+
+namespace BotStateSample
+{
+    public class BotStateDemo : IBot
+    {
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = new CancellationToken())
+        {
+            await turnContext.SendActivityAsync("Hello Bot SDK 4.0", cancellationToken: cancellationToken);
+        }
+    }
+}
