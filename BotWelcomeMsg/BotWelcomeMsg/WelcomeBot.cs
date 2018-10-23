3bdd5e0c11b5875c1f04c096e48d5caa65c1bca4
Vinoth Rajendran <r.vinoth@live.com>
2018-10-23 19:09:53 +0530

    Send welcome message to the user using adaptive card

---
+++ BotWelcomeMsg/BotWelcomeMsg/WelcomeBot.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Bot.Builder;
+
+namespace BotWelcomeMsg
+{
+    public class WelcomeBot : IBot
+    {
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = new CancellationToken())
+        {
+            await turnContext.SendActivityAsync("Hello Welcome to Bot",cancellationToken:cancellationToken);
+        }
+    }
+}
